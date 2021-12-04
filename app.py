de1c5358c20a0e945fbfedfe9478d552ccddff35
kwc0596 <54780612+kwc0596@users.noreply.github.com>
2021-12-03 21:37:27 -0800

    updated app.py

--- app.py
+++ app.py
@@ -4,22 +4,23 @@ from threading import Thread
 from service import wiki_query
 
 api = "http://api.quotable.io/random"
-quotes=[]
 quoteinfo=[]
+quotes=[]
+
 zero=0
 quote_number=zero
 
 window=tk.Tk()
+
 window.geometry ("1000x400")
-window.title ("Random Quote Generator")
-window.grid_columnconfigure (0, weight = 1)
+
 window.configure (bg="black")
 
 def preload_quotes():
     global quotes
 
     print("Loading")
-    for x in range(15):
+    for quoteNum in range(15) :
         random_quote=requests.get(api). json()
         content=random_quote ["content"]
         author=random_quote ["author"]
@@ -28,44 +29,61 @@ def preload_quotes():
         print (content)
         quotes.append (quote)
     print("Loading is finished.")
+window.title ("Random Quote Generator")
+
+window.grid_columnconfigure (0, weight = 1)
+
+
+
 
 preload_quotes ()
 
 def get_random_quote ():
-    global quote_label
+    
     global quote_wiki
+
     global quotes
+
+    global quote_label
+
     global quote_number
     
 
     quote_label.configure (text = quotes[quote_number])
-    info = wiki_query( quoteinfo [quote_number] )
-    quote_wiki.configure (text = info)
-    quote_number = quote_number + 1
-    print (quote_number)
 
-    if quotes[quote_number]==quotes[-3]:
+    info = wiki_query( quoteinfo [quote_number] )
+
+    quote_wiki.configure (text = info)
+
+    quote_number = quote_number + 1
+
+
+    if quotes [quote_number] == quotes [-3] :
+
         thread = Thread (target = preload_quotes)
+
         thread.start ()
 
 
 
  
 # Design of frontend
-quote_label = tk.Label(window, text="You can click on this button to create a random #",
-                        height=8,
+quote_label = tk.Label (window, text="You can click on this button to create a random #",
                         pady=20,
                         wraplength=1000,
-                        font=("Helvetica", 15))
-quote_label.grid(row=0, column=0, stick="WE", padx=20, pady=10)
+                        height=8,
+                        
+                        
+                        font =("Arial", 15))
+quote_label.grid(row = 0, stick ="WE", padx=20,column=0, pady = 10)
 quote_wiki = tk.Label(window)
-quote_wiki.grid(row=0, column=0, stick="S", padx=20, pady=10)
+quote_wiki.grid(row=0,  stick="S", padx=20, column=0, pady=10)
 
-button = tk.Button(text = "Generate a Quote", command=get_random_quote,bg='#0052cc', fg="#ffffff",
-                    activebackground="black", font=("Helvetica", 15))
-button.grid(row=1, column=0, stick="WE", padx=20, pady=10)
+button = tk.Button (text = "Generate a Quote",fg = "#948f8f", command = get_random_quote,bg = '#0052dd', 
+                    activebackground = "black", font = ("Arial", 15))
+button.grid (row = 1, padx = 20, column = 0, stick = "WE",  pady = 10)
 
 #Execute the program
 
 if __name__=="__main__" :
-    window.mainloop()
+    window.mainloop ()
